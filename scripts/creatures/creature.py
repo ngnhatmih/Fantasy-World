3570143e5528b1c38b8b21faa92acbf5d7bfe193
ngnhatmih <ngnhatmih9997@gmail.com>
2022-12-31 13:48:26 +0700

    1st commit

---
+++ scripts/creatures/creature.py
@@ -0,0 +1,14 @@
+class Creature:
+    def __init__(self, name, sprite, level, exp, hit_points, strength, agility, defense, accuracy, critical_hit, critical_hit_chance):
+        self.name = name
+        self.sprite = sprite
+        self.level = level
+        self.exp = exp
+        self.hit_points = hit_points
+        self.strength = strength #physical power or damage output.
+        self.agility = agility #speed or reflexes
+        self.defense = defense
+        self.accuracy = accuracy
+        self.critical_hit = critical_hit #critical damage
+        self.critical_hit_chance = critical_hit_chance
+        
