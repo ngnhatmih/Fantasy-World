891b56d3532f2968a7bb9bec2ee69ed5d723c3fb
ngnhatmih <ngnhatmih9997@gmail.com>
2023-01-01 02:21:47 +0700

    This is a message

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
 import pygame
 from constant import *
+from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, KEYDOWN, K_ESCAPE, QUIT)
 
 pygame.init()
-screen = pygame.display.set_mode(size, 0, 32)
+screen = pygame.display.set_mode(size)
 
 isRunning = True
 i = 0
 while(isRunning):
-    i+=1
-    if i == 255:
-        i = 0
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning == False
+        
     
     screen.fill((i,0,255))
 
-    pygame.display.flip()
+    pygame.display.update()
 
 pygame.quit()
