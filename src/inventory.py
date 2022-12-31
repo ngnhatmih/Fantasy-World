97772a809afe6d7fb326dd9ce95c36bdd022bff5
ngn <111894593+ngnhatmih@users.noreply.github.com>
2022-12-31 19:20:25 +0700

    Update inventory.py

--- src/inventory.py
+++ src/inventory.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-from entities.entity import Item
-=======
-from entities.item import Item
->>>>>>> 4c75d4f66d3036e20e8a842df6004f98a90c4eea
-
 class Inventory:
     def __init__(self, capcity):
         self.capcity = capcity
