90cd5395354abde0fee935e7c9970a4fb71128ea
ngnhatmih <ngnhatmih9997@gmail.com>
2023-01-03 00:48:47 +0700

    I UPDATED PYTHON

--- src/main.py
+++ src/main.py
@@ -3,37 +3,64 @@ from constant import *
 
 class FantasyWorld:
 
-    def __init__(self):
+    def __init__(self, size):
 
+        # Note myself in future checking whether each command (intialize, window creation, ...) suceed
+
+        # Initialize SDL
         sdl2.ext.init()
 
-        self.window = sdl2.ext.Window("Title", size)
+        # Create the window and renderer
+        self.window = sdl2.ext.Window("Fantasy World", size)
         self.renderer = sdl2.ext.renderer.Renderer(self.window, flags = sdl2.SDL_RENDERER_ACCELERATED)
-        
-
-    def run(self):
 
         self.window.show()
+
         self.isRunning = True
-        while self.isRunning:
-            for event in sdl2.ext.get_events():
-                if event.type == sdl2.SDL_Quit:
+
+    # Event handling function
+    def handleEvents(self):
+
+        for event in sdl2.ext.get_events():
+            match event.type:
+                case sdl2.SDL_Quit:
                     self.isRunning = False
+                
 
-            self.renderer.color = sdl2.ext.Color(*colors['white'], 0)
-            self.renderer.clear()
-            self.renderer.color = sdl2.ext.Color(*colors['violet'], 255)
+    # Render function
+    def render(self):
 
-            self.renderer.fill((100,100,100,100))
+        self.renderer.color = sdl2.ext.Color(*colors['white'], 0)
+        self.renderer.clear()
+        self.renderer.color = sdl2.ext.Color(*colors['violet'], 255)
 
-            self.renderer.present()
+        self.renderer.fill((100,100,100,100))
 
-            self.window.refresh()
+        
 
+    def update(self):
+
+        self.renderer.present()
+        self.window.refresh()
+
+
+    # Clean up
+    def clean(self):
+         
         self.renderer.destroy()
         self.window.close()
         sdl2.ext.quit()
 
 if __name__ == "__main__":
-    game = FantasyWorld()
-    game.run()
+
+    # Initilize with a certain size
+    game = FantasyWorld(size)
+
+    # Game loop
+    while game.isRunning:
+        
+        game.handleEvents()
+        game.render()
+        game.update()
+    
+    game.clean()
