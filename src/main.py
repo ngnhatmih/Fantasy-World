b8f6efb015e9f2f7ebab334582e7b0b39f8c9191
ngnhatmih <ngnhatmih9997@gmail.com>
2023-01-02 17:57:15 +0700

    main function idiom

--- src/main.py
+++ src/main.py
@@ -1,31 +1,39 @@
 import sdl2, sdl2.ext
 from constant import *
 
-sdl2.ext.init()
+class FantasyWorld:
 
-window = sdl2.ext.Window("Title", size)
-renderer = sdl2.ext.renderer.Renderer(window, flags = sdl2.SDL_RENDERER_ACCELERATED)
+    def __init__(self):
 
-window.show()
+        sdl2.ext.init()
 
-isRunning = True
-while isRunning:
-
-    for event in sdl2.ext.get_events():
-        if event.type == sdl2.SDL_Quit:
-            isRunning = False
+        self.window = sdl2.ext.Window("Title", size)
+        self.renderer = sdl2.ext.renderer.Renderer(self.window, flags = sdl2.SDL_RENDERER_ACCELERATED)
         
-    renderer.color = sdl2.ext.Color(*colors['white'], 0)
-    renderer.clear()
-    renderer.color = sdl2.ext.Color(*colors['violet'], 255)
 
-    renderer.fill((100,100,100,100))
+    def run(self):
 
-    renderer.present()
+        self.window.show()
+        self.isRunning = True
+        while self.isRunning:
+            for event in sdl2.ext.get_events():
+                if event.type == sdl2.SDL_Quit:
+                    self.isRunning = False
 
-    window.refresh()
+            self.renderer.color = sdl2.ext.Color(*colors['white'], 0)
+            self.renderer.clear()
+            self.renderer.color = sdl2.ext.Color(*colors['violet'], 255)
 
-renderer.destroy()
-window.close()
-sdl2.ext.quit()
-    
+            self.renderer.fill((100,100,100,100))
+
+            self.renderer.present()
+
+            self.window.refresh()
+
+        self.renderer.destroy()
+        self.window.close()
+        sdl2.ext.quit()
+
+if __name__ == "__main__":
+    game = FantasyWorld()
+    game.run()
