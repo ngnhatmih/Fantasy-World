a6d9c04e340579d9ace50e1622b4cbf0c6f9dac4
Ark1409 <codeark1409@gmail.com>
2023-01-04 20:48:52 -0800

    fixed texture manager

--- src/texture_manager.py
+++ src/texture_manager.py
@@ -1,58 +1,38 @@
-import sdl2, sdl2.ext, sdl2.sdlimage
+import sdl2
+import sdl2.ext
+import sdl2.sdlimage
 import ctypes
 import pathlib
 import sys
 
-m_textureMap = {}
-pTexture = None
+
+textureMap = {}
+
 
 class TextureManager:
     def __init__(self):
         pass
-    
 
     @staticmethod
-    def load(path: str, id: str, pRenderer: sdl2.ext.renderer.Renderer)->bool:
+    def load(path: str, id: str, renderer: sdl2.ext.renderer.Renderer) -> bool:
+        # Use the built-in pysdl2 image loading library instead of the C version
+        # Still returns the C SDL_Surface though
+        surface: sdl2.SDL_Surface = sdl2.ext.image.load_img(path)
 
+        if surface == 0:
+            return False
 
-        pTexture: sdl2.ext.renderer.Texture = sdl2.sdlimage.IMG_LoadTexture(pRenderer.sdlrenderer, path)
+        textureMap[id] = sdl2.ext.Texture(renderer, surface)
+        return True
 
-        if pTexture != 0:
-            m_textureMap[id] = pTexture
-            return True
-        else: return False
 
     @staticmethod
-    def draw(id: str, x: int, y: int, width: int, height: int, scale: float, r: float, pRenderer: sdl2.ext.renderer.Renderer, flip: sdl2.SDL_RendererFlip):
-        srcRect: sdl2.SDL_Rect = sdl2.SDL_RectEmpty
-        destRect: sdl2.SDL_Rect = sdl2.SDL_RectEmpty
+    def draw(renderer: sdl2.ext.renderer.Renderer, id: str, x: int, y: int, width: int, height: int, currentRow: int = 0, currentFrame: int = 0, scaleX: float = 1, scaleY: float = 1):
+        srcRect = (width*currentFrame, height*currentRow, width, height)
+        destRect = (x, y, width*scaleX, height*scaleY)
 
-        srcRect.x = 0
-        srcRect.y = 0
-        srcRect.w = 0
-        srcRect.h = 0
-        destRect.x = 0
-        destRect.y = 0
-        destRect.w *= scale
-        destRect.h *= scale
-
-        pRenderer.copy(pTexture,srcRect,destRect)
+        renderer.copy(textureMap[id], srcRect, destRect)
 
     @staticmethod
-    def drawFrame(id: str, x: int, y: int, width: int, height: int, scale: float, currentRow: int, currentFrame: int, r: float, pRenderer: sdl2.ext.renderer.Renderer, flip: sdl2.SDL_RendererFlip):
-        srcRect: sdl2.SDL_Rect = sdl2.SDL_RectEmpty
-        destRect: sdl2.SDL_Rect = sdl2.SDL_RectEmpty
-        srcRect.x = width*currentFrame
-        srcRect.y = height*currentRow
-        srcRect.w = destRect.w = width
-        srcRect.h = destRect.h = height
-        destRect.x = x
-        destRect.y = y
-        destRect.w *= scale
-        destRect.h *= scale
-
-        pRenderer.copy(pTexture,srcRect,destRect)
-
-    @staticmethod
-    def cleanFromTextureMap(id : str):
-        m_textureMap.pop(id)
+    def cleanFromTextureMap(id: str):
+        textureMap.pop(id)
